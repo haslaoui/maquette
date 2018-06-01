32298558388ab420b1f7b314741cae71414bb80a
HASSAN <HASSAN@tbs.dev.com>
2018-06-01 11:41:46 +0200

    Inital commit

---
+++ maquette1/src/com/test/maquette/main/MaquetteMain.java
@@ -0,0 +1,24 @@
+/*
+ * @(#)MaquetteMain.java	1.0		le 1 juin 2018.
+ */
+package com.test.maquette.main;
+
+/**
+ * test
+ *
+ * @author HASSAN
+ * @version 1.0
+ * @since 1.0
+ */
+public class MaquetteMain {
+    public MaquetteMain() {
+    }
+
+    public static void main(String[] args) {
+        try {
+            System.err.println("Maquette 1 !!");
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
